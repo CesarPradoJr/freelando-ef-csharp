5320c125f3db8dde11f7fa26473b158598f98758
Cesar <cesarhenrique_prado@hotmail.com>
2025-07-29 14:15:39 -0300

    Feat: Added CRUD on endpoints

--- Solution1/src/Freelando.Api/Endpoints/ClienteExtension.cs
+++ Solution1/src/Freelando.Api/Endpoints/ClienteExtension.cs
@@ -1,4 +1,5 @@
-﻿using Freelando.Dados;
+﻿using Freelando.Api.Requests;
+using Freelando.Dados;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Freelando.Api.Endpoints;
@@ -7,11 +8,51 @@ public static class ClienteExtension
 {
     public static void AddEndPointClientes(this WebApplication app)
     {
-        app.MapGet("/clientes", async ([FromServices] ClienteConverter converter, [FromServices] FreelandoContext contexto) =>
+        app.MapGet("/clientes", async ([FromServices] ClienteConverter converter, [FromServices] FreelandoContext context) =>
         {
-            var clientes = converter.EntityListToResponseList(contexto.Clientes.ToList());
-            var entries = contexto.ChangeTracker.Entries();
+            var clientes = converter.EntityListToResponseList(context.Clientes.ToList());
+            var entries = context.ChangeTracker.Entries();
             return Results.Ok(await Task.FromResult(clientes));
+        }).WithTags("Cliente").WithOpenApi();
+
+        app.MapPost("/cliente", async ([FromServices] ClienteConverter converter, [FromServices] FreelandoContext context, ClienteRequest clienteRequest) =>
+        {
+            var cliente = converter.RequestToEntity(clienteRequest);
+            await context.Clientes.AddAsync(cliente);
+            await context.SaveChangesAsync();
+
+            return Results.Created($"/cliente/{cliente.Id}", cliente);
+        }).WithTags("Cliente").WithOpenApi();
+
+        app.MapPut("/clientes/{id}", async ([FromServices] ClienteConverter converter, [FromServices] FreelandoContext context, Guid id, ClienteRequest clienteRequest) =>
+        {
+            var cliente = await context.Clientes.FindAsync(id);
+            if(cliente is null)
+            {
+                return Results.NotFound();
+            }
+            var clienteAtualizado = converter.RequestToEntity(clienteRequest);
+            cliente.Nome = clienteAtualizado.Nome;
+            cliente.Email = clienteAtualizado.Email;
+            cliente.Telefone = clienteAtualizado.Telefone;
+            cliente.Cpf = clienteAtualizado.Cpf;
+
+            await context.SaveChangesAsync();
+
+            return Results.Ok(cliente);
+        }).WithTags("Cliente").WithOpenApi();
+
+        app.MapDelete("/cliente/{id}", async ([FromServices] ClienteConverter converter, [FromServices] FreelandoContext context, Guid id) =>
+        {
+            var cliente = await context.Clientes.FindAsync(id);
+            if ( cliente is null)
+            {
+                return Results.NotFound();
+            }
+            context.Clientes.Remove(cliente);
+            await context.SaveChangesAsync();
+
+            return Results.NoContent();
         }).WithTags("Cliente").WithOpenApi();
     }
 }
--- Solution1/src/Freelando.Api/Endpoints/ServicoExtension.cs
+++ Solution1/src/Freelando.Api/Endpoints/ServicoExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Freelando.Api.Converters;
+using Freelando.Api.Requests;
 using Freelando.Dados;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,43 @@ public static class ServicoExtensions
         {
             var servico = converter.EntityListToResponseList(context.Servicos.ToList());
             return Results.Ok(await Task.FromResult(servico));
+        }).WithTags("Servico").WithOpenApi();
+
+        app.MapPost("/servico", async ([FromServices] ServicoConverter converter, [FromServices] FreelandoContext context, ServicoRequest servicoRequest) =>
+        {
+            var servico = converter.RequestToEntity(servicoRequest);
+            await context.Servicos.AddAsync(servico);
+            await context.SaveChangesAsync();
+
+            return Results.Created($"/servico/{servico.Id}", servico);
+        }).WithTags("Servico").WithOpenApi();
+
+        app.MapPut("/servico/{id}", async ([FromServices] ServicoConverter converter, [FromServices] FreelandoContext context, Guid id, ServicoRequest servicoRequest) =>
+        {
+            var servico = await context.Servicos.FindAsync(id);
+            if(servico is null)
+            {
+                return Results.NotFound();
+            }
+            var servicoAtualizado = converter.RequestToEntity(servicoRequest);
+            servico.Descricao = servicoAtualizado.Descricao;
+            servico.Titulo =servicoAtualizado.Titulo;
+            await context.SaveChangesAsync();
+
+            return Results.Ok(await Task.FromResult(servico));
+        }).WithTags("Servico").WithOpenApi();
+
+        app.MapDelete("/servico/{id}", async ([FromServices] ServicoConverter converter, [FromServices] FreelandoContext context, Guid id) =>
+        {
+            var servico = await context.Servicos.FindAsync(id);
+            if(servico is null)
+            {
+                return Results.NotFound();
+            }
+            context.Servicos.Remove(servico);
+            await context.SaveChangesAsync();
+
+            return Results.NoContent();
         }).WithTags("Servico").WithOpenApi();
     }
 }
